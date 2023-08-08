76a5ff583897bb29e4e56babb228ecdceee973af
ERuvinga <ruvingaelie@gmail.com>
2023-08-08 12:18:42 +0200

    adding medias animports poppin Font familly

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,14 @@ import {BookmarkIcon} from '@heroicons/react/24/outline'
 export default function Home() {
   return (
     <>
-      <Head></Head>
-      <p className='text-[#ff0] p-[10px] border'>
+      <Head/>
+      <p className='text-[#00f] p-[10px]'>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
         Nisi nemo ratione praesentium debitis nam consequuntur 
         tempora blanditiis quisquam molestias tenetur!
       </p>
 
-      <BookmarkIcon className='w-[25px] text-[#00f]'/>
+      <BookmarkIcon className='w-[20px] text-[#00f]'/>
     </>
   )
 }
